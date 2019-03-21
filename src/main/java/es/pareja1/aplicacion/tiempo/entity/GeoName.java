5bcd6159dbd855f7d1334e04a8162bc24d6931d1
Goferello <48448021+Goferello@users.noreply.github.com>
2019-03-21 17:27:14 +0100

    1727:Nuevo

--- src/main/java/es/pareja1/aplicacion/tiempo/entity/GeoName.java
+++ src/main/java/es/pareja1/aplicacion/tiempo/entity/GeoName.java
@@ -10,24 +10,13 @@ public class GeoName {
 	private String asciiName;// Nombre de la ciudad a buscar
 	private Bbox bbox;
 
-	private final String pattern1 = "http://api.geonames.org/searchJSON?q=";
-	private final String pattern2 = "&maxRows=20&startRow=0&lang=en&isNameRequired =true&style=FULL&username=ilgeonamessample";
-
-
-	public GeoName () {}
+	public GeoName() {
+	}
 
 	public GeoName(String asciiName, Bbox bbox) {
 		super();
 		this.asciiName = asciiName;
 		this.bbox = bbox;
-	}
-
-	public String getPattern1() {
-		return pattern1;
-	}
-
-	public String getPattern2() {
-		return pattern2;
 	}
 
 	public String getAsciiName() {
@@ -46,9 +35,5 @@ public class GeoName {
 		this.bbox = bbox;
 	}
 
-	@Override
-	public String toString() {
-		return pattern1 + asciiName + pattern2;
-	}
 
 }

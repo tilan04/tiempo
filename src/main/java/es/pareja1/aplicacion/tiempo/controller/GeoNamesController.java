cddf0ffc102bdf1dab8609de77eb037699bfc33e
Goferello <48448021+Goferello@users.noreply.github.com>
2019-03-25 17:23:04 +0100

    dede

--- src/main/java/es/pareja1/aplicacion/tiempo/controller/GeoNamesController.java
+++ src/main/java/es/pareja1/aplicacion/tiempo/controller/GeoNamesController.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +30,15 @@ public class GeoNamesController {
 	@Resource
 	private GeoNamesService geoNamesService;
 
+	@Autowired
+	ResultWeatherObservationServiceImpl weather;
+
 	private List<String> listaBusquedas = new ArrayList<String>();
+
+//	 @GetMapping("/index")
+//	    String index(Principal principal) {
+//	        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+//	    }
 
 	@RequestMapping("/index") // Indicamos la URL con la que se ejecuta
 	public ModelAndView getListGeoNames() {
@@ -64,14 +75,22 @@ public class GeoNamesController {
 //		return asciiName;
 //	}
 
-	@RequestMapping(value = "/resultado", method = RequestMethod.POST)
-	public String greetingSubmit(String nombre, Model model, ResultWeatherObservationServiceImpl weather) {
+	@RequestMapping(value = "/resultado", method = {RequestMethod.POST, RequestMethod.GET})
+	public String greetingSubmit(@Valid @ModelAttribute("ciudadBuscada")String nombre, Model model, BindingResult errorBinding) {
 
 		model.addAttribute("ciudadBuscada", nombre);
 		TotalResultsCount r = geoNamesService.getListGeonames(nombre);
-
+		if (r.getGeonames().isEmpty()) {
+			model.addAttribute("error", "sin ciudades");
+			 return "pages/index";
+		}
 		Bbox b = r.getGeonames().get(0).getBbox();
 		List<WeatherObservations> listaObservaciones = geoNamesService.getListWeatherObservation(b);
+		if (listaObservaciones.isEmpty()) {
+			model.addAttribute("error", "sin estacion metereologica");
+			 return "pages/index";
+		}
+			
 		model.addAttribute("temperaturaMedia", weather.getMediaTemperaturas(listaObservaciones));
 		model.addAttribute("humedadMedia", weather.getMediaHumedad(listaObservaciones));
 		model.addAttribute("vientoMedio", weather.getMediaViento(listaObservaciones));
@@ -79,8 +98,17 @@ public class GeoNamesController {
 		model.addAttribute("geoName", r.getGeonames().get(0));
 
 //    		model.addAttribute("stationName",weather.getStationName(listaObservaciones));
+		
+	    if (errorBinding.hasErrors()) {
+	        return "signup";
+	    }
+
+	    //do some processin
 		if (listaObservaciones != null) {
 			listaBusquedas.add(nombre);
+			for (String ciudad : listaBusquedas) {
+				System.out.println(ciudad.toString());
+			}
 		}
 		return "pages/tiempo";
 	}
@@ -88,7 +116,6 @@ public class GeoNamesController {
 	@RequestMapping(value = "")
 	public String getHome(HttpSession session) {
 		session.setAttribute("mySession", listaBusquedas);
-
 		return "pages/index";
 	}
 
